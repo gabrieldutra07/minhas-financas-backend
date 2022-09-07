1c36a4d4d2e1740a7d5ebc04846f304aa72e7187
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-07 18:56:25 -0300

    implementado o método de autenticação do usuário

--- minhas-financas/src/main/java/com/example/demo/model/repository/UsuarioRepository.java
+++ minhas-financas/src/main/java/com/example/demo/model/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.entity.Usuario;
@@ -7,4 +9,6 @@ import com.example.demo.model.entity.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	boolean existsByEmail(String email);
+	
+	Optional<Usuario> findByEmail (String email);
 }
--- minhas-financas/src/main/java/com/example/demo/service/impl/UsuarioServiceImpl.java
+++ minhas-financas/src/main/java/com/example/demo/service/impl/UsuarioServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.demo.service.impl;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exception.ErroAutenticacao;
 import com.example.demo.exception.RegraNegocioException;
 import com.example.demo.model.entity.Usuario;
 import com.example.demo.model.repository.UsuarioRepository;
@@ -23,8 +26,17 @@ public class UsuarioServiceImpl implements UsuarioService {
 
 	@Override
 	public Usuario autenticar(String email, String senha) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Usuario> usuario = repository.findByEmail(email);
+		
+		if(!usuario.isPresent()) {
+			throw new ErroAutenticacao("Usuário não encontrado!");
+		}
+		
+		if(!usuario.get().getSenha().equals(senha)) {
+			throw new ErroAutenticacao("Senha inválida!");
+		}
+			
+		return usuario.get();
 	}
 
 	@Override
