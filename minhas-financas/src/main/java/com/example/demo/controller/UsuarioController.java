953b3044488df702a64ad385ccda3e31916a70fe
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-17 14:06:12 -0300

    criado o primeiro método no controller

--- minhas-financas/src/main/java/com/example/demo/controller/UsuarioController.java
+++ minhas-financas/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,21 +1,45 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.UsuarioDTO;
+import com.example.demo.model.entity.Usuario;
+import com.example.demo.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
 	
+	private UsuarioService service;
+	
+	public UsuarioController(UsuarioService service) {
+		this.service = service;
+	}
 	
 	
+	@PostMapping
 	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
-		return null;
+		
+		Usuario usuario = Usuario.builder()
+				.nome(dto.getNome())
+				.senha(dto.getSenha())
+				.email(dto.getEmail()).build();
+		
+		try {
+		
+			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+		
+		} catch(Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
 	}
 
 }
