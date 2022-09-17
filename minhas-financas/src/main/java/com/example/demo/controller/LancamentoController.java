d161cc9ff5392f9d89e965bd34638cd16b4b483a
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-17 17:15:45 -0300

    implementado método de buscar os lançamentos

--- minhas-financas/src/main/java/com/example/demo/controller/LancamentoController.java
+++ minhas-financas/src/main/java/com/example/demo/controller/LancamentoController.java
@@ -1,12 +1,19 @@
 package com.example.demo.controller;
 
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.LancamentoDTO;
@@ -58,6 +65,14 @@ public class LancamentoController {
 		
 	}
 	
+	@DeleteMapping("{id}")
+	public ResponseEntity deletar(@PathVariable("id") Long id) {
+		return service.obterPorId(id).map(entity -> {
+			service.deletar(entity);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado!", HttpStatus.BAD_REQUEST));
+	}
+	
 	private Lancamento converter(LancamentoDTO dto) {
 		Lancamento lancamento = new Lancamento();
 		lancamento.setId(dto.getId());
@@ -74,6 +89,30 @@ public class LancamentoController {
 		lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));
 		
 		return lancamento;
+	}
+	
+	@GetMapping
+	public ResponseEntity buscar(
+			@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano,
+			@RequestParam("usuario") Long idUsuario
+			) {
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(ano);
+		lancamentoFiltro.setAno(ano);
+		
+		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
+		if(!usuario.isPresent()) {
+			return ResponseEntity.badRequest().body("Não foi possível realizar a consulta!");
+		} else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+		
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
+		
 	}
 	
 }
