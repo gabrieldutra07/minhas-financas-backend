d3da7aa52c0d6bcace3606b9c607486f22d5fc4a
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-17 16:00:19 -0300

    criada a interface da service de lancamento, assim como a implementacao do metodo salvar

---
+++ minhas-financas/src/main/java/com/example/demo/service/LancamentoService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.model.entity.Lancamento;
+import com.example.demo.model.enums.StatusLancamento;
+
+import java.util.List;
+
+public interface LancamentoService {
+
+	Lancamento salvar(Lancamento lancamento);
+	Lancamento atualizar(Lancamento lancamento);
+	void deletar(Lancamento lancamento);
+	List<Lancamento> buscar(Lancamento lancamentoFiltro);
+	void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+}
---
+++ minhas-financas/src/main/java/com/example/demo/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.entity.Lancamento;
+import com.example.demo.model.enums.StatusLancamento;
+import com.example.demo.model.repository.LancamentoRepository;
+import com.example.demo.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService{
+	
+	private LancamentoRepository repository;
+	
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	@Transactional
+	public Lancamento salvar(Lancamento lancamento) {
+		
+		return repository.save(lancamento);
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
