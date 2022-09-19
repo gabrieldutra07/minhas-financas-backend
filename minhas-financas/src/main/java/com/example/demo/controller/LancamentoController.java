f0e48429be306a430ba5d611a1939b46920db195
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-19 20:42:38 -0300

    criado o endpoint para atualizaçao do status do lançamento

--- minhas-financas/src/main/java/com/example/demo/controller/LancamentoController.java
+++ minhas-financas/src/main/java/com/example/demo/controller/LancamentoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.AtualizaStatusDTO;
 import com.example.demo.dto.LancamentoDTO;
 import com.example.demo.exception.RegraNegocioException;
 import com.example.demo.model.entity.Lancamento;
@@ -93,6 +94,24 @@ public class LancamentoController {
 		}
 		
 		return lancamento;
+	}
+	
+	@PutMapping("{id}/atualiza-status")
+	public ResponseEntity atualizarStatus(@PathVariable("id") Long id, @RequestBody AtualizaStatusDTO dto) {
+		
+		return service.obterPorId(id).map(entity -> {
+			StatusLancamento statusSelecionado = StatusLancamento.valueOf(dto.getStatus());
+			if(statusSelecionado == null) {
+				return ResponseEntity.badRequest().body("Não foi possível atualizar o status do lançamento!");
+			} try {
+				entity.setStatus(statusSelecionado);
+				service.atualizar(entity);
+				return ResponseEntity.ok(entity);
+			} catch (RegraNegocioException e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado!", HttpStatus.BAD_REQUEST));
+		
 	}
 	
 	@GetMapping
