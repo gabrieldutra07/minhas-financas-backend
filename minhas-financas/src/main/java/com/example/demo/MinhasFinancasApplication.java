0504b1f09f8bc2a279406c74ad814b0741d8be1a
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-10-09 13:54:03 -0300

    retirada da classe de testes para build

--- minhas-financas/src/main/java/com/example/demo/MinhasFinancasApplication.java
+++ minhas-financas/src/main/java/com/example/demo/MinhasFinancasApplication.java
@@ -9,11 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @EnableWebMvc
 public class MinhasFinancasApplication implements WebMvcConfigurer {
-	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasFinancasApplication.class, args);
